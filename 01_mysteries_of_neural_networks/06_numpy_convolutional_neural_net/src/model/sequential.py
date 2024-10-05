8c43a475f9ab6eb51e222a17609b71e0996279b3
Peonyxz <102782880+Peonyxz@users.noreply.github.com>
2024-10-05 18:38:02 +0700

    Created using Colab

---
+++ 01_mysteries_of_neural_networks/06_numpy_convolutional_neural_net/src/model/sequential.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNkcKT+1ISO2KmUpN76KhMo",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Peonyxz/Machine_Learning/blob/main/01_mysteries_of_neural_networks/06_numpy_convolutional_neural_net/src/model/sequential.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "UH1qZwr0b0iQ"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
